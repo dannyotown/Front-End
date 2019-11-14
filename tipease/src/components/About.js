86d27012c2db26cb6a96637fe8add48065bea84c
Germaine Pannell <me@germainep.com>
2019-11-13 20:53:03 -0500

    basic setup for tream member card
    may break out into smaller components

---
+++ tipease/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About () => {
+  return (
+    <div>
+      <h1>About the team</h1>
+      <div>
+        <img/>
+        <h2>Name</h2>
+        <p>Praesent facilisis velit nisi, at tempor ante pharetra non. Pellentesque cursus posuere neque ac consequat. Fusce vulputate leo ac enim varius, quis hendrerit risus aliquam. Sed pharetra nunc a odio auctor congue.</p>
+      </div>
+    </div>
+  )
+}
