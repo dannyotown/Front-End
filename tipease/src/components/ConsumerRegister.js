39e4e4efd0f920f112940e5581ad7406a6eef019
Jackie Atwood <takalaj@verizon.net>
2019-11-14 19:27:11 -0500

    First push to GitHub JaxAtwood 7:27pm EST

---
+++ tipease/src/components/ConsumerRegister.js
@@ -0,0 +1,11 @@
+//JaxAtwood
+
+import React from "react";
+
+export default function ConsumerRegister() {
+    return (
+        <div>
+            <p>C Register</p>
+        </div>
+    )
+}
---
+++ tipease/src/components/SWRegister.js
@@ -0,0 +1,11 @@
+//JaxAtwood
+
+import React from "react";
+
+export default function SWRegister() {
+    return (
+        <div>
+            <p>SW Register</p>
+        </div>
+    )
+}
