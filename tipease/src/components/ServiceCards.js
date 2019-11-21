5ee1fad482fa18b08c3b22b46bde1d037d65c4ac
Dtown <dannyotown@gmail.com>
2019-11-20 18:13:41 -0600

    Service Cards

--- tipease/src/components/ServiceCards.js
+++ tipease/src/components/ServiceCards.js
@@ -2,20 +2,27 @@ import React from "react";
 import styled from "styled-components";
 
 export default function ServiceCards({ worker }) {
+  console.log(worker);
   return (
     <ServiceCard>
       <ServiceCardHeader>
         {worker.FirstName} {worker.LastName}
       </ServiceCardHeader>
       <ServiceCardInfo>
-        <ServiceInfo>{worker.tagline}</ServiceInfo>
+        <ServiceInfo>Company: {worker.company}</ServiceInfo>
+        <ServiceInfo>Years at Company: {worker.YearsAtCompany}</ServiceInfo>
+        <ServiceInfo>Email: {worker.email}</ServiceInfo>
+        <ServiceInfo>
+          Current Balance: ${worker.balance !== null ? worker.balance : "0"}
+        </ServiceInfo>
+        <UpdateTip>Update Tip</UpdateTip>
       </ServiceCardInfo>
     </ServiceCard>
   );
 }
 const ServiceCard = styled.div`
   width: 45%;
-  height: 300px;
+  height: 225px;
   margin: 0 auto;
   background-color: #fafafa;
   border: 2px solid #3dcccc;
@@ -28,12 +35,18 @@ const ServiceCardHeader = styled.div`
   border-bottom: 1px solid #3dcccc;
   padding: 1%;
 `;
-
 const ServiceCardInfo = styled.div`
   width: 90%;
   margin-top: 1%;
   margin: 0 auto;
 `;
+const UpdateTip = styled.button`
+  margin: auto;
+  width: 100%;
+  height: 25px;
+  margin-top: 1%;
+`;
+
 const ServiceInfo = styled.div`
   padding: 1%;
   font-size: 1rem;
